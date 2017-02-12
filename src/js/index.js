08631dfee9de4694f3a53c3eeded81bc27849337
Jitendra Pal <jitendra.pal@jasperindia.local>
2017-02-12 16:41:34 +0530

    Updated

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,14 @@
         $(".content").addClass("hidden");
         $("#"+showCtId).removeClass("hidden");
     });
+
+    $(".org-name").on("click", function(){
+        if($(this).is(".open")){
+            $(this).removeClass("open").addClass("close");
+            $(this).find("~ .projects").addClass("hidden");
+        } else {
+            $(this).removeClass("close").addClass("open");
+            $(this).find("~ .projects").removeClass("hidden");
+        }
+    });
 })();
