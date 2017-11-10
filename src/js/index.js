2ff567f5ef055cc92ebc8539179306c8f2ffbfe0
jitendra <jitendra.pal@snapdeal.com>
2017-11-11 00:52:18 +0530

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -16,4 +16,16 @@
             $(this).find("~ .projects").removeClass("hidden");
         }
     });
+
+    // update experience
+    function updateExp(){
+        var joinDate = new Date('2012-07-16');
+        var totalExp = new Date(new Date() - joinDate);
+        var expMsg = ((totalExp.toISOString().slice(0, 4) - 1970) + " Years " + (totalExp.getMonth()+1) + " Months " + totalExp.getDate() + " Days");
+        
+        $(".dynamic-exp").text(expMsg);
+    }
+
+    updateExp();
+    
 })();
