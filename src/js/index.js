1765bc409c3abbb92b212fa26c09aefb414443ed
jitendra <jitendra.pal@snapdeal.com>
2018-07-09 12:57:58 +0530

    updated date logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
-(function(){
-    $(".nav-list li").on("click", function(){
+(function () {
+    $(".nav-list li").on("click", function () {
         $(".nav-list li").removeClass("selected");
         $(this).addClass("selected");
         var showCtId = $(this).data("id");
         $(".content").addClass("hidden");
-        $("#"+showCtId).removeClass("hidden");
+        $("#" + showCtId).removeClass("hidden");
     });
 
-    $(".org-name").on("click", function(){
-        if($(this).is(".open")){
+    $(".org-name").on("click", function () {
+        if ($(this).is(".open")) {
             $(this).removeClass("open").addClass("close");
             $(this).find("~ .projects").addClass("hidden");
         } else {
@@ -18,14 +18,26 @@
     });
 
     // update experience
-    function updateExp(){
+    function updateExp() {
+        var todayDate = new Date();
         var joinDate = new Date('2012-07-16');
-        var totalExp = new Date(new Date() - joinDate);
-        var expMsg = ((totalExp.toISOString().slice(0, 4) - 1970) + " Years " + (totalExp.getMonth()+1) + " Months " + totalExp.getDate() + " Days");
-        
+        var expMsg="";
+
+        var y = todayDate.getYear() - joinDate.getYear();
+        var m = todayDate.getMonth() - joinDate.getMonth();
+
+        if (m < 0) {
+            y--;
+            m = 12 + m;
+        }
+        expMsg = y +" Years ";
+        if(m > 0){
+            expMsg +=  m + " Months";
+        }
+
         $(".dynamic-exp").text(expMsg);
     }
 
-    //updateExp();
-    
+    updateExp();
+
 })();
