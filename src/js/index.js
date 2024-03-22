25afb81a14ec67eb45c1544d869ae8548a0042ea
jpjitendrapal <jpjitendrapal@gmail.com>
2024-03-22 23:35:05 +0530

    Updated to add scroll with smooth

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,72 @@
 (function () {
-    $(".nav-list li").on("click", function () {
-        $(".nav-list li").removeClass("selected");
-        $(this).addClass("selected");
-        var showCtId = $(this).data("id");
-        $(".content").addClass("hidden");
-        $("#" + showCtId).removeClass("hidden");
-    });
+  $(".nav-list li").on("click", function () {
+    $(".nav-list li").removeClass("selected");
+    $(this).addClass("selected");
+    var showCtId = $(this).data("id");
+    document.getElementById(showCtId).scrollIntoView({ behavior: "smooth" });
+  });
 
-    $(".org-name").on("click", function () {
-        if ($(this).is(".open")) {
-            $(this).removeClass("open").addClass("close");
-            $(this).find("~ .projects").addClass("hidden");
-        } else {
-            $(this).removeClass("close").addClass("open");
-            $(this).find("~ .projects").removeClass("hidden");
+  $(".org-name").on("click", function () {
+    if ($(this).is(".open")) {
+      $(this).removeClass("open").addClass("close");
+      $(this).find("~ .projects").addClass("hidden");
+    } else {
+      $(this).removeClass("close").addClass("open");
+      $(this).find("~ .projects").removeClass("hidden");
+    }
+  });
+
+  var jumpLinks = document.querySelectorAll(".jump-link");
+  var sections = document.querySelectorAll(".content");
+
+  // Create a new Intersection Observer
+  var observer = new IntersectionObserver(
+    function (entries, observer) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          // Add 'active' class to corresponding nav link
+          console.log("entry: ", '[data-id="' + entry.target.id + '"]');
+          var targetLink = document.querySelector(
+            '[data-id="' + entry.target.id + '"]'
+          );
+          jumpLinks.forEach(function (link) {
+            link.classList.remove("selected");
+          });
+          if (targetLink) {
+            targetLink.classList.add("selected");
+          }
         }
-    });
+      });
+    },
+    { threshold: 0.5 }
+  ); // Intersection threshold of 100%
 
-    // update experience
-    function updateExp() {
-        var todayDate = new Date();
-        var joinDate = new Date('2012-07-16');
-        var expMsg="";
+  // Observe each section
+  sections.forEach(function (section) {
+    observer.observe(section);
+  });
+  console.log("Executing");
 
-        var y = todayDate.getYear() - joinDate.getYear();
-        var m = todayDate.getMonth() - joinDate.getMonth();
+  // update experience
+  function updateExp() {
+    var todayDate = new Date();
+    var joinDate = new Date("2012-07-16");
+    var expMsg = "";
 
-        if (m < 0) {
-            y--;
-            m = 12 + m;
-        }
-        expMsg = y +" Years ";
-        if(m > 0){
-            expMsg +=  m + " Months";
-        }
+    var y = todayDate.getYear() - joinDate.getYear();
+    var m = todayDate.getMonth() - joinDate.getMonth();
 
-        $(".dynamic-exp").text(expMsg);
+    if (m < 0) {
+      y--;
+      m = 12 + m;
+    }
+    expMsg = y + " Years ";
+    if (m > 0) {
+      expMsg += m + " Months";
     }
 
-    updateExp();
+    $(".dynamic-exp").text(expMsg);
+  }
 
+  updateExp();
 })();
